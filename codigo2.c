c1f33e8af7bf1de3c1d560d01dd4207669d6af49
Breno <brenomoreirag@gmail.com>
2018-09-17 15:56:25 -0300

    Codigo 2

---
+++ codigo2.c
@@ -0,0 +1,25 @@
+ 
+void soma(int a, int b)
+{
+    printf("A soma: %d\n", (a + b));
+}
+
+void subt(int a, int b)
+{
+    printf("A subtracao: %d\n", (a - b));
+}
+
+void prod(int a, int b)
+{
+    printf("O produto: %d\n", (a * b));
+}
+
+void divi(int a, int b)
+{
+    printf("A divisao: %.2f\n", ((float)a / b));
+}
+
+void resto(int a, int b)
+{
+    printf("O resto: %d\n", (a % b));
+}
